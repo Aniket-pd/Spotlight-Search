a37a255af0f9699a44cb7523c6442038e385b7b3
Aniket-pd <71229270+Aniket-pd@users.noreply.github.com>
2025-10-18 14:27:41 +0530

    Add slash menu and navigation filters

--- src/background/index.js
+++ src/background/index.js
@@ -5,6 +5,7 @@ import { createTabActions } from "./tabs.js";
 import { createCommandExecutor } from "./commands.js";
 import { createFaviconService } from "./favicons.js";
 import { registerMessageHandlers } from "./messages.js";
+import { createNavigationService, registerNavigationListeners } from "./navigation.js";
 import {
   registerLifecycleEvents,
   registerCommandShortcuts,
@@ -16,12 +17,16 @@ const context = createBackgroundContext({ buildIndex });
 const tabActions = createTabActions();
 const executeCommand = createCommandExecutor({ tabActions, scheduleRebuild: context.scheduleRebuild });
 const { resolveFaviconForTarget } = createFaviconService({ cache: context.faviconCache });
+const navigation = createNavigationService();
+
+registerNavigationListeners(navigation);
 
 registerMessageHandlers({
   context,
   runSearch,
   executeCommand,
   resolveFaviconForTarget,
+  navigation,
 });
 
 registerLifecycleEvents(context);
